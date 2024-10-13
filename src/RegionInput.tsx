51b8ba2a62c81fc7816e27efed76274fa4b82520
gimohc <mrlenovog50@gmail.com>
2024-10-13 22:40:31 +0300

    fixed regions added all options

--- src/RegionInput.tsx
+++ src/RegionInput.tsx
@@ -14,10 +14,23 @@ export const RegionInput = forwardRef<HTMLSelectElement>(function RegionInput(
         name="region"
         onChange={(e) => setRegion(e.target.value)}
       >
-        <option value="americas"> Americas </option>
-        <option value="europe"> Europe </option>
-        <option value="asia"> Asia </option>
-        <option value="sea"> Oceania </option>
+        <option value="americas"> NA </option>
+        <option value="americas"> BR </option>
+        <option value="americas"> LAN </option>
+        <option value="americas"> LAS </option>
+        <option value="europe"> EUNE </option>
+        <option value="europe"> EUW </option>
+        <option value="europe"> ME </option>
+        <option value="europe"> TR </option>
+        <option value="europe"> RU </option>
+        <option value="asia"> KR </option>
+        <option value="asia"> JP </option>
+        <option value="sea"> OCE </option>
+        <option value="sea"> PH </option>
+        <option value="sea"> SG </option>
+        <option value="sea"> TH </option>
+        <option value="sea"> TW </option>
+        <option value="sea"> VN </option>
       </select>
     </>
   );
